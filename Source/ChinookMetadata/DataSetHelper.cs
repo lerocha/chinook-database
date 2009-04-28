e1b2ae37a6aa33ec836d0ca8b5128da02dda258b
lerocha <none@none>
2009-04-28 05:04:29 +0000

    Auto-generating the main T4 template file (chinook.t4) from the ChinookMetadata assembly and refactoring existing T4 templates to include Chinook.t4.
    --HG--
    extra : convert_revision : svn%3A620c4292-3293-3848-9960-d3592df64433/Trunk%4019

---
+++ Source/ChinookMetadata/DataSetHelper.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace ChinookMetadata
+{
+    class DataSetHelper
+    {
+        public static string GetVersionNumber()
+        {
+            return "1.1";
+        }
+
+        public static string GetOracleType(DataColumn col)
+        {
+            switch (col.DataType.ToString())
+            {
+                case "System.String":
+                    return string.Format("VARCHAR2({0})", col.MaxLength);
+                case "System.Int32":
+                    return "NUMBER";
+                case "System.Decimal":
+                    return "NUMBER";
+                case "System.DateTime":
+                    return "DATE";
+                default:
+                    return "error_" + col.DataType;
+            }
+        }
+
+        public static string GetSqlServerType(DataColumn col)
+        {
+            switch (col.DataType.ToString())
+            {
+                case "System.String":
+                    return string.Format("NVARCHAR({0})", col.MaxLength);
+                case "System.Int32":
+                    return "INTEGER";
+                case "System.Decimal":
+                    return "NUMERIC(10,2)";
+                case "System.DateTime":
+                    return "DATETIME";
+                default:
+                    return "error_" + col.DataType;
+            }
+        }
+
+        public static string GetMySqlType(DataColumn col)
+        {
+            switch (col.DataType.ToString())
+            {
+                case "System.String":
+                    return string.Format("VARCHAR({0})", col.MaxLength);
+                case "System.Int32":
+                    return "INTEGER";
+                case "System.Decimal":
+                    return "NUMERIC(10,2)";
+                case "System.DateTime":
+                    return "DATE";
+                default:
+                    return "error_" + col.DataType;
+            }
+        }
+
+        public static bool IsLastCreateTableElement(DataTable table, DataColumn col)
+        {
+            if ((col == table.Columns[table.Columns.Count - 1]) && (table.Constraints.Count == 0))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets a string containing the primary key of the given table. For example "CustomerId" or "PlaylistId, TrackId".
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns>A string representing the primary key (single or composite).</returns>
+        public static string GetPrimaryKeyString(DataTable table)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (Constraint constraint in table.Constraints)
+            {
+                UniqueConstraint uniqueConstraint = constraint as UniqueConstraint;
+
+                if (uniqueConstraint != null && uniqueConstraint.IsPrimaryKey)
+                {
+                    foreach (DataColumn pk in uniqueConstraint.Columns)
+                    {
+                        if (sb.Length != 0) sb.Append(", ");
+                        sb.Append(pk.ColumnName);
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Gets the name of the primary key constraint, for example "PK_Customer".
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns>Primary key constraint name.</returns>
+        public static string GetPrimaryKeyConstraintName(DataTable table)
+        {
+            foreach (Constraint constraint in table.Constraints)
+            {
+                UniqueConstraint uniqueConstraint = constraint as UniqueConstraint;
+
+                if (uniqueConstraint != null && uniqueConstraint.IsPrimaryKey)
+                {
+                    return uniqueConstraint.ConstraintName;
+                }
+            }
+            return string.Empty;
+        }
+
+        public static string GetExpectedValue(DataColumn col, string value)
+        {
+            string expected;
+            if (col.DataType == typeof(DateTime))
+            {
+                expected = string.Format("Convert.ToDateTime(\"{0}\").ToString()", value);
+            }
+            else
+            {
+                expected = string.Format("\"{0}\"", value);
+            }
+
+            return expected;
+        }
+
+    }
+}
